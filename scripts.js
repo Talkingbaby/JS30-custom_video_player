9203070aa027bda1ca85976c740aaa68cc5e6a47
kyle kenney <kairukenney@gmail.com>
2017-08-09 10:28:59 -0700

    skip buttons working

--- scripts.js
+++ scripts.js
@@ -25,8 +25,15 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // add our event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach( button => button.addEventListener('click', skip));
